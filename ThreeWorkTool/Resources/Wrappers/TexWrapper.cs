503c0abe6c7f3b62efba1b0b6fa12ed0a5fe75f7
EternalYoshi <43284309+EternalYoshi@users.noreply.github.com>
2021-03-13 11:47:35 -0500

    Thanks to TGE, I got the cfg filled with all the resource hashes. hashes with unkown type names will be exported with an extension that's the hash itself. Also started to build TexWrapper for texture support.

---
+++ ThreeWorkTool/Resources/Wrappers/TexWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThreeWorkTool.Resources.Archives;
+
+namespace ThreeWorkTool.Resources.Wrappers
+{
+    public class TexWrapper:ArcEntryWrapper
+    {
+        public string Magic;
+        public int XSize;
+        public int YSize;
+        public int ZSize;
+        public int version;
+        public string TexType;
+        public bool HasTransparency;
+        public bool HasMips;
+        public int MipCount;
+
+
+
+
+    }
+}
