29ea1b2b60a1316baa33d6df961301f3827bd7ec
EternalYoshi <43284309+EternalYoshi@users.noreply.github.com>
2021-11-17 09:30:33 -0500

    Just enlarged the font in the text box used for RPL and CST files.

--- ThreeWorkTool/FrmMain.Designer.cs
+++ ThreeWorkTool/FrmMain.Designer.cs
@@ -332,6 +332,7 @@
             // 
             this.txtRPList.AcceptsReturn = true;
             this.txtRPList.AcceptsTab = true;
+            this.txtRPList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtRPList.Location = new System.Drawing.Point(4, 0);
             this.txtRPList.Multiline = true;
             this.txtRPList.Name = "txtRPList";
