0c9e770402de6cf7365ead41e10ff036d54a76de
EternalYoshi <43284309+EternalYoshi@users.noreply.github.com>
2023-02-04 18:51:15 -0500

    Version Bump.

--- ThreeWorkTool/Properties/AssemblyInfo.cs
+++ ThreeWorkTool/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ThreeWorkTool")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2021, 2022, 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
