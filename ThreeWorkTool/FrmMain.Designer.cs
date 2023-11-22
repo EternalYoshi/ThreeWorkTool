3950747dd11dd32833a5fa3fd559c98ac9fca999
EternalYoshi <43284309+EternalYoshi@users.noreply.github.com>
2023-11-21 22:49:05 -0500

    Version Bump.

--- ThreeWorkTool/FrmMain.Designer.cs
+++ ThreeWorkTool/FrmMain.Designer.cs
@@ -555,7 +555,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(384, 256);
             this.Name = "FrmMainThree";
-            this.Text = "ThreeWork Tool V0.71";
+            this.Text = "ThreeWork Tool V0.72";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMainThree_FormClosing);
             this.Load += new System.EventHandler(this.FrmMainThree_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmMainThree_KeyDown);
